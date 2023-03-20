48dcb5ebf3599403a9a99c8166bc8b311802a9c9
김준석 <114639257+Jun1115@users.noreply.github.com>
2023-03-20 14:20:04 +0900

    Delete 반복문 directory

--- 반복문/Baekjoon2438.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-Star = 0 
-while Star < N :
-    Star = Star + 1
-    print("*"*Star)
--- 반복문/Baekjoon2439.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-Star = 0
-for i in range(1, N+1):
-    Star = i
-    print(str("*"*Star).rjust(N))
