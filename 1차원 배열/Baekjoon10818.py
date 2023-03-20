99611dcfa32b50124f3ccd00db5ceea019fd1b90
김준석 <114639257+Jun1115@users.noreply.github.com>
2023-03-20 14:19:40 +0900

    Delete 1차원 배열 directory

--- 1차원 배열/Baekjoon10818.py
+++
@@ -1,3 +0,0 @@
-N = int(input())
-listttt = list(map(int, input().split()))
-print(str(min(listttt)), str(max(listttt)))
--- 1차원 배열/Baekjoon10871.py
+++
@@ -1,8 +0,0 @@
-N = list(map(int, input().split()))
-M = list(map(int, input().split()))
-L = list()
-for i in range(N[0]):
-    if N[1] > M[i]:
-        L.append(M[i])
-
-print(' '.join(map(str,L)))
